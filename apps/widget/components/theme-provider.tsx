6bddc91733c49fcffa1c3a4d361aa95da2858d05
DeepGandhi07 <149680753+DGStacknyu@users.noreply.github.com>
2026-08-13 10:03:05 +0200

    11: Added Jotai State management for widget screen router

--- apps/widget/components/theme-provider.tsx
+++ apps/widget/components/theme-provider.tsx
@@ -1,15 +1,19 @@
 "use client"
 
 import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { Provider } from "jotai"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import * as React from "react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 
 function ThemeProvider({
   children,
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>
+  return (
+    <ConvexProvider client={convex}>
+      <Provider>{children}</Provider>
+    </ConvexProvider>
+  )
 }
 
 export { ThemeProvider }
